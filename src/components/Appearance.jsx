1896cb83528620974af9001f7241fb67889fb240
Lucas Livchits <livchitsl@gmail.com>
2021-05-20 17:51:16 -0300

    Extract Appearance to a component

---
+++ src/components/Appearance.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import { PROPERTIES_TO_GET_AVERAGE } from '../constants';
+import { convertCmToFeetAndInches, convertKgToLb, getAverageOf } from '../utils';
+
+function Appearance({ team }) {
+  const averages = getAverageOf(team, PROPERTIES_TO_GET_AVERAGE);
+
+  return (
+    <article className='px-4'>
+      <h2 className='mt-4 text-2xl font-bold sm:mt-0'>Average Appearance</h2>
+      <ul className='px-1 mt-2'>
+        {Object.keys(averages).map((property) => {
+          const value = averages[property];
+          return (
+            <li key={property} className='flex justify-between w-4/6 sm:w-full'>
+              <span className='uppercase'>{property} </span>
+              <span className='text-gray-500'>
+                {property === 'height'
+                  ? `${value} cm / ${convertCmToFeetAndInches(value)}`
+                  : `${value} kg / ${convertKgToLb(value)} lb`}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </article>
+  );
+}
+
+Appearance.propTypes = { team: PropTypes.array.isRequired };
+
+export default Appearance;
--- src/components/TeamStats.jsx
+++ src/components/TeamStats.jsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 
-import { team, PROPERTIES_TO_GET_AVERAGE } from '../constants';
-import { getAverageOf, convertCmToFeetAndInches, convertKgToLb } from '../utils';
+import { team } from '../constants';
 
+import Appearance from './Appearance';
 import Powerstats from './Powerstats';
 
 function TeamStats() {
-  const averages = getAverageOf(team, PROPERTIES_TO_GET_AVERAGE);
-
   return (
-    <section className='max-w-4xl py-8 m-2 mx-auto text-blue-600 bg-gray-200 rounded-md bg-opacity-40'>
+    <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
       <div className='text-center'>
         <h1 className='inline-block px-12 py-3 mx-auto mb-6 text-3xl font-semibold bg-blue-600 rounded-full text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
           Team Stats
@@ -17,24 +15,7 @@ function TeamStats() {
       </div>
       <div className='sm:flex'>
         <Powerstats team={team} />
-        <article className='px-4'>
-          <h2 className='mt-4 text-2xl font-bold sm:mt-0'>Average Appearance</h2>
-          <ul className='px-1 mt-2'>
-            {Object.keys(averages).map((property) => {
-              const value = averages[property];
-              return (
-                <li key={property} className='flex justify-between w-4/6 sm:w-full'>
-                  <span className='uppercase'>{property} </span>
-                  <span className='text-gray-500'>
-                    {property === 'height'
-                      ? `${value} cm / ${convertCmToFeetAndInches(value)}`
-                      : `${value} kg / ${convertKgToLb(value)} lb`}
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
-        </article>
+        <Appearance team={team} />
       </div>
     </section>
   );
