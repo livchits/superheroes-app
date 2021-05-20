bc9c3e1ec615957b8017eb1d55710de3135a0ad3
Lucas Livchits <livchitsl@gmail.com>
2021-05-20 00:48:11 -0300

    Add logic to show average appearance

--- src/components/TeamStats.jsx
+++ src/components/TeamStats.jsx
@@ -1,27 +1,33 @@
 import * as React from 'react';
 
-import { team } from '../constants';
-import getHueValue from '../utils/getHueValue';
-import getSortedPowerstatsTotals from '../utils/getSortedPowerstatsTotals';
+import { team, PROPERTIES_TO_GET_AVERAGE } from '../constants';
+import {
+  getHueValue,
+  getSortedPowerstatsTotals,
+  getAverageOf,
+  convertCmToFeetAndInches,
+  convertKgToLb,
+} from '../utils';
 
 function TeamStats() {
   const sortedPowerstatsTotals = getSortedPowerstatsTotals(team);
   const getHueForMaxTeamLength = getHueValue(team.length * 100);
+  const averages = getAverageOf(team, PROPERTIES_TO_GET_AVERAGE);
 
   return (
-    <section className='text-center'>
-      <h1 className='inline-block px-6 py-3 mx-auto mt-8 mb-6 text-3xl bg-opacity-50 rounded-full text-gray-50 bg-cyan-600 ring-8 ring-opacity-80 ring-gray-200'>
-        Your team stats
-      </h1>
-      <h2 className='ml-4 text-2xl text-left text-opacity-50 text-cyan-600'>
-        Total Powerstats
-      </h2>
-      <div className='px-4 mt-2 text-opacity-70 text-cyan-600'>
+    <section className='text-cyan-600'>
+      <div className='text-center'>
+        <h1 className='inline-block px-6 py-3 mx-auto mt-8 mb-6 text-3xl bg-opacity-50 rounded-full text-gray-50 bg-cyan-600 ring-8 ring-opacity-80 ring-gray-200'>
+          Your team stats
+        </h1>
+      </div>
+      <h2 className='ml-4 text-2xl text-opacity-50'>Total Powerstats</h2>
+      <div className='px-4 my-2 text-opacity-70'>
         <table>
           <tbody>
             {sortedPowerstatsTotals.map(([powerstat, total]) => (
               <tr key={powerstat}>
-                <td className='px-1 text-left capitalize'>{powerstat}</td>
+                <td className='px-1 capitalize'>{powerstat}</td>
                 <td className='px-1 text-right '>{total}</td>
                 <td className='w-full px-1'>
                   <div className='bg-gray-300 h-4 mx-auto rounded-lg p-0.5'>
@@ -41,6 +47,21 @@ function TeamStats() {
           </tbody>
         </table>
       </div>
+      <h2 className='ml-4 text-2xl text-opacity-50'>Appearance</h2>
+      <ul className='ml-4'>
+        {Object.keys(averages).map((property) => {
+          const value = averages[property];
+
+          return (
+            <li key={property}>
+              <span className='capitalize'>{property}: </span>
+              {property === 'height'
+                ? `${value} cm / ${convertCmToFeetAndInches(value)}`
+                : `${value} kg / ${convertKgToLb(value)} lb`}
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
