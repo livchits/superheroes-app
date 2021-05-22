f72299fe830760edb75082a96bd8fbf8c728c223
Lucas Livchits <livchitsl@gmail.com>
2021-05-22 00:25:24 -0300

    Extract results to a component

--- src/components/SuperheroSearch.jsx
+++ src/components/SuperheroSearch.jsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import useGetSuperheros from '../hooks/useGetSuperheros';
 
+import SearchResults from './SearchResults';
+
 function SuperheroSearch() {
   const [query, setQuery] = React.useState(null);
-  const { status, data, error } = useGetSuperheros(query);
+  const { status, data: superheros, error } = useGetSuperheros(query);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,40 +15,36 @@ function SuperheroSearch() {
   };
 
   return (
-    <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
-      <div className='text-center'>
-        <h1 className='inline-block py-3 mx-auto mb-6 text-3xl font-semibold bg-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
-          Find Superheros
-        </h1>
-      </div>
-      <form className='max-w-lg px-4 mx-auto ' onSubmit={handleSubmit}>
-        <label className='pl-0.5 text-lg' htmlFor='search'>
-          Enter a superhero name
-        </label>
-        <div className='flex justify-center mt-0.5'>
-          <input
-            className='flex-1 pl-2 text-gray-500 border-2 border-gray-400 rounded-xl focus:ring-blue-500 focus:ring-2 focus:outline-none focus:border-transparent placeholder-opacity-80'
-            id='search'
-            name='search'
-            placeholder='Ex: "Batman"'
-            type='text'
-          />
-          <button
-            className='p-2 px-4 ml-2 text-lg transition-colors duration-300 border-2 border-blue-600 rounded-xl focus:ring-blue-500 focus:ring-2 focus:outline-none focus:border-transparent hover:text-gray-100 hover:bg-blue-600 active:bg-blue-500'
-            type='submit'
-          >
-            Search
-          </button>
+    <>
+      <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
+        <div className='text-center'>
+          <h1 className='inline-block py-3 mx-auto mb-6 text-3xl font-semibold bg-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
+            Find Superheros
+          </h1>
         </div>
-      </form>
-      {data && (
-        <ul>
-          {data.map((superhero) => (
-            <li key={superhero.id}>{superhero.fullName}</li>
-          ))}
-        </ul>
-      )}
-    </section>
+        <form className='max-w-lg px-4 mx-auto ' onSubmit={handleSubmit}>
+          <label className='pl-0.5 text-lg' htmlFor='search'>
+            Enter a superhero name
+          </label>
+          <div className='flex justify-center mt-0.5'>
+            <input
+              className='flex-1 pl-2 text-gray-500 border-2 border-gray-400 rounded-xl focus:ring-blue-500 focus:ring-2 focus:outline-none focus:border-transparent placeholder-opacity-80'
+              id='search'
+              name='search'
+              placeholder='Ex: "Batman"'
+              type='text'
+            />
+            <button
+              className='p-2 px-4 ml-2 text-lg transition duration-300 border-2 border-blue-600 rounded-xl focus:ring-blue-500 focus:ring-2 focus:outline-none focus:border-transparent hover:text-gray-100 hover:bg-blue-600 active:bg-blue-500'
+              type='submit'
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </section>
+      {superheros && <SearchResults superheros={superheros} />}
+    </>
   );
 }
 
