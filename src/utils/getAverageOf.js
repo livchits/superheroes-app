8337a5a371f65a15dfbbc97ca7f5ee16bcd6e2f5
Lucas Livchits <livchitsl@gmail.com>
2021-05-23 21:03:30 -0300

    Change superhero structure and getAverageOf to match with it

--- src/utils/getAverageOf.js
+++ src/utils/getAverageOf.js
@@ -1,10 +1,27 @@
-export function getAverageOf(team, appearanceProperties) {
+/* export function getAverageOf(team, appearanceProperties) {
   const averages = {};
 
   appearanceProperties.forEach((property) => {
     const average =
       team.reduce((total, { appearance }) => {
         const value = Number(appearance[property][1].split(' ')[0]);
+        return (total += value);
+      }, 0) / team.length;
+
+    const roundAverage = Math.round(average);
+    averages[property] = roundAverage;
+  });
+
+  return averages;
+} */
+
+export function getAverageOf(team, appearanceProperties) {
+  const averages = {};
+
+  appearanceProperties.forEach((property) => {
+    const average =
+      team.reduce((total, superhero) => {
+        const value = Number(superhero[property][1].split(' ')[0]);
         return (total += value);
       }, 0) / team.length;
 
