45e4d3a58e649518a82cf7f0e04f48b6e9f505d8
Lucas Livchits <livchitsl@gmail.com>
2021-05-26 12:10:10 -0300

    Add remove functionality

--- src/components/SuperheroCard.jsx
+++ src/components/SuperheroCard.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 import PowerstatsForCard from './PowerstatsForCard';
 
-function SuperheroCard({ superhero }) {
-  const { name, imageUrl, powerstats } = superhero;
+function SuperheroCard({ superhero, handleRemove }) {
+  const { name, imageUrl, powerstats, id } = superhero;
 
   return (
     <li className='w-11/12 mx-auto my-2 border-2 border-gray-300 shadow-sm text-opacity-70 rounded-xl sm:w-72 sm:mx-0 md:mx-1'>
@@ -23,6 +23,7 @@ function SuperheroCard({ superhero }) {
         <button
           className='w-24 px-3 text-lg font-semibold leading-normal transition-colors duration-300 border-2 border-blue-600 shadow-md rounded-xl focus:ring-blue-500 focus:ring-2 focus:outline-none focus:border-transparent hover:text-gray-100 bg-gradient-to-l hover:from-blue-500 hover:to-blue-600'
           type='submit'
+          onClick={() => handleRemove(id)}
         >
           Remove
         </button>
@@ -32,7 +33,8 @@ function SuperheroCard({ superhero }) {
 }
 
 SuperheroCard.propTypes = {
-  superhero: PropTypes.object,
+  superhero: PropTypes.object.isRequired,
+  handleRemove: PropTypes.func.isRequired,
 };
 
 export default SuperheroCard;
--- src/components/SuperheroesList.jsx
+++ src/components/SuperheroesList.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 import SuperheroCard from './SuperheroCard';
 
-function SuperheroesList({ team }) {
+function SuperheroesList({ setTeam, team }) {
+  const removeSuperhero = (idToRemove) => {
+    setTeam((previousTeam) => previousTeam.filter(({ id }) => id !== idToRemove));
+  };
+
   return (
     <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
       <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
@@ -11,13 +15,20 @@ function SuperheroesList({ team }) {
       </h1>
       <ul className='sm:flex sm:flex-wrap sm:justify-evenly md:justify-start md:ml-1'>
         {team.map((superhero) => (
-          <SuperheroCard key={superhero.id} superhero={superhero} />
+          <SuperheroCard
+            key={superhero.id}
+            handleRemove={removeSuperhero}
+            superhero={superhero}
+          />
         ))}
       </ul>
     </section>
   );
 }
 
-SuperheroesList.propTypes = { team: PropTypes.array.isRequired };
+SuperheroesList.propTypes = {
+  team: PropTypes.array.isRequired,
+  setTeam: PropTypes.func.isRequired,
+};
 
 export default SuperheroesList;
