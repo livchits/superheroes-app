6b3845672c9cb1fcbae3f4f3cd5327e25ab1f851
Lucas Livchits <livchitsl@gmail.com>
2021-05-25 22:13:54 -0300

    Remove unnecessary divs

--- src/components/SuperheroesSearch.jsx
+++ src/components/SuperheroesSearch.jsx
@@ -23,11 +23,9 @@ function SuperheroesSearch({ setTeam, team }) {
     <>
       <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
         {error && <ErrorSearch handleClose={handleClose} message={error.message} />}
-        <div className='text-center'>
-          <h1 className='inline-block py-3 mx-auto mb-6 text-3xl font-semibold bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
-            Find Superheroes
-          </h1>
-        </div>
+        <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
+          Find Superheroes
+        </h1>
         <form className='max-w-lg px-4 mx-auto ' onSubmit={handleSubmit}>
           <label className='pl-0.5 text-lg' htmlFor='search'>
             Enter a superhero name
