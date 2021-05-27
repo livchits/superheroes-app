87f145e1e8b10c13e5e5fe08215cd9a57b91d97e
Lucas Livchits <livchitsl@gmail.com>
2021-05-26 22:59:15 -0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
+import PrivateRoute from './components/PrivateRoute';
+import { UserProvider } from './context/UserContext';
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 function App() {
-  return <Home />;
+  return (
+    <UserProvider>
+      <Router>
+        <Switch>
+          <PrivateRoute path='/home'>
+            <Home />
+          </PrivateRoute>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Redirect to='/login' />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
+  );
 }
 
 export default App;
