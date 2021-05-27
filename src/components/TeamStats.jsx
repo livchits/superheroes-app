f10546dc5bbd0f9af3e7012001e67527960839df
Lucas Livchits <livchitsl@gmail.com>
2021-05-27 12:45:55 -0300

    Use better html tags

--- src/components/TeamStats.jsx
+++ src/components/TeamStats.jsx
@@ -5,7 +5,7 @@ import Appearance from './Appearance';
 import Powerstats from './Powerstats';
 function TeamStats({ team }) {
   return (
-    <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
+    <article className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
       <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
         Team Stats
       </h1>
@@ -15,13 +15,13 @@ function TeamStats({ team }) {
           <Appearance team={team} />
         </div>
       ) : (
-        <article className='max-w-lg px-3 mx-auto text-2xl font-bold text-center'>
+        <section className='max-w-lg px-3 mx-auto text-2xl font-bold text-center'>
           <p>Your team has no members.</p>
           <p className='mb-2 font-normal'>Search superheroes and add them to the team.</p>
           <p> Have fun!</p>
-        </article>
+        </section>
       )}
-    </section>
+    </article>
   );
 }
 
