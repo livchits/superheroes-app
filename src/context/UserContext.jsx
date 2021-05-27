f57e308842817543f71a2aa53335887e5fb32099
Lucas Livchits <livchitsl@gmail.com>
2021-05-26 23:00:54 -0300

    Add context to manage user

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const UserContext = React.createContext();
+
+function UserProvider(props) {
+  const [user, setUser] = React.useState(
+    () => JSON.parse(window.sessionStorage.getItem('superheroes_app_user')) || null
+  );
+
+  return <UserContext.Provider value={[user, setUser]} {...props} />;
+}
+
+function useUser() {
+  const [user, setUser] = React.useContext(UserContext);
+
+  return [user, setUser];
+}
+
+export { UserProvider, useUser };
