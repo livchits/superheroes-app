3eafbb3f07377fb03be5bde1c9b1f89c73f71357
Lucas Livchits <livchitsl@gmail.com>
2021-05-27 23:18:59 -0300

    Fix SuperheroDerails styles

--- src/components/SuperheroDetails.jsx
+++ src/components/SuperheroDetails.jsx
@@ -33,7 +33,7 @@ function SuperheroDetails({ superhero, handleClose }) {
         >
           <XIcon className='w-6 mx-auto text-gray-600 transition-colors duration-200 group-hover:text-gray-300' />
         </button>
-        <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
+        <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50'>
           {name}
         </h1>
         <div className='flex mx-auto'>
@@ -41,31 +41,31 @@ function SuperheroDetails({ superhero, handleClose }) {
           <PowerstatsForCard powerstats={powerstats} />
         </div>
         <ul className='mx-2 text-lg leading-tight'>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Weight: </span>
             {`${weight[0]} / ${weight[1]}`}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Height: </span>
             {`${height[0]} / ${height[1]}`}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Full name: </span>
             {`${fullName}`}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Aliases: </span>
             {formatList(aliases)}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Eye color: </span>
             {`${eyeColor}`}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Hair color: </span>
             {`${hairColor}`}
           </li>
-          <li className='mb-1'>
+          <li className='mb-1.5'>
             <span className='font-bold uppercase'>Work place: </span>
             {`${workPlace}`}
           </li>
