963b77d061ced8c2c398096864dcd07ab28b8307
Lucas Livchits <livchitsl@gmail.com>
2021-05-27 23:14:27 -0300

    Fix SuperheroesList styles

--- src/components/SuperheroesList.jsx
+++ src/components/SuperheroesList.jsx
@@ -9,8 +9,8 @@ function SuperheroesList({ setTeam, team }) {
   };
 
   return (
-    <section className='w-11/12 py-8 mx-auto my-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-40'>
-      <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50 ring-4 ring-opacity-80 ring-gray-200'>
+    <section className='w-11/12 py-8 mx-auto mt-2 text-blue-600 bg-gray-200 rounded-md shadow-sm md:mx-auto md:max-w-4xl bg-opacity-90'>
+      <h1 className='py-3 mx-auto mb-6 text-3xl font-semibold text-center w-max bg-gradient-to-l from-blue-500 to-blue-600 px-7 rounded-xl text-gray-50'>
         Your team
       </h1>
       <ul className='sm:flex sm:flex-wrap sm:justify-evenly md:justify-start md:ml-1'>
